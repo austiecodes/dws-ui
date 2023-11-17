4a48f182037068a303a1682f2715988a9001f9f3
austiecodes <way24h+gh@gmail.com>
2023-11-17 17:14:48 +0800

    brach: dev

--- src/main.jsx
+++ src/main.jsx
@@ -3,51 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./routes/ErrorPage.jsx";
-import Dashboard from "./routes/Dashboard.jsx";
-import Machines from "./pages/Machines.jsx";
-import AllUsers from "./pages/Admin/AllUsers.jsx";
-import Toggle from "./components/Base/Toggle.jsx";
-import Features from "./pages/Admin/Features.jsx";
-import Settings from "./pages/Settings.jsx";
-import Containers from "./pages/Containers.jsx";
+
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
 		errorElement: <ErrorPage />,
-	},
-	{
-		path: "/dashboard",
-		element: <Dashboard />,
-		children: [
-			{
-				path: "gpuinfo",
-				element: <Machines />,
-			},
-			{
-				path: "containers",
-				element: <Containers />,
-			},
-			{
-				path: "allusers",
-				element: <AllUsers />,
-			},
-			{
-				path: "features",
-				element: <Features />,
-			},
-			{
-				path: "settings",
-				element: <Settings />,
-			}
-		],
-	},
-	{
-		path: "/test",
-		element: <Toggle />,
-	},
+	}, 
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
